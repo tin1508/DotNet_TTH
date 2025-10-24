8b3f13d82b542f0a04f72eff8fb242205c1dc629
mhoang <nguyenhoang150805@gmail.com>
2025-10-24 23:28:21 +0700

    first commit

---
+++ Lab_01/AnimalManagement/AnimalManagement/Program.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using MainData;
+
+namespace AnimalManagement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //a. Tạo danh sách gồm 3 con chó và 2 con mèo
+            int nDog = 3, nCat = 2;
+            Animal[] animalList = new Animal[nDog + nCat];
+            //b. Nhập và xuất thông tin của các con vật
+            for(int i = 0; i < nDog; i++)
+            {
+                Animal dog = new Dog();
+                dog.InputInfo();
+                animalList[i] = dog;
+            }
+            for(int i = 0; i < nCat; i++)
+            {
+                Animal cat = new Cat();
+                cat.InputInfo();
+                animalList[nDog + i] = cat;
+            }
+            //xuất  thông tin của các con vật ra
+            foreach(Animal animal in animalList)
+            {
+                animal.DisplayInfo();
+            }
+            Console.ReadLine();
+        }
+    }
+}
+
+namespace MainData
+{
+    //c.bắt lỗi khi nhập tuổi âm hoặc lớn hơn 20
+    public class InvalidNumException: Exception
+    {
+        public InvalidNumException(string message) : base(message) { }
+    }
+    public abstract class Animal
+    {
+        protected string name;
+        protected int age;
+        protected double height, weight; //weight tính theo đơn vị kg
+        public Animal() { }
+        public Animal(string name, int age, double height, double weight)
+        {
+            this.name = name;
+            this.age = age;
+            this.height = height;
+            this.weight = weight;
+        }
+        public abstract void InputInfo();
+        public abstract void DisplayInfo();
+        public abstract void InputAge();
+        public abstract void InputHeight();
+        public abstract void InputWeight();
+        public override string ToString()
+        {
+            return "name = " + this.name + ", Age = " + this.age
+                    + ", Height = (cm)" + this.height + ", Weight = (kg)" + this.weight + "\n";
+        }
+       
+    }
+    public class Dog : Animal
+    {
+        
+        public Dog() : base() { }
+        public Dog(string name, int age, int height, double weight) : base(name, age, height, weight) { }
+        
+
+        public override void InputInfo()
+        {
+            Console.Write("Input Dog name: ");
+            this.name = Console.ReadLine();
+            InputAge();
+            InputHeight();
+            InputWeight();
+        }
+        public override void InputAge()
+        {
+            while (true)
+            {
+                try
+                {
+                    Console.Write("Input Dog Age: ");
+                    age = int.Parse(Console.ReadLine());
+                    if (age <= 0 || age > 20) throw new InvalidNumException("Age must be between 1 and 20.");
+                    break;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Not input a Number. Please reinput a number!");
+                }
+                catch (InvalidNumException e)
+                {
+                    Console.WriteLine("Invalid Number: {0}", e.Message);
+                }
+            }
+        }
+        //d. Bắt lỗi khi người dùng nhập chuỗi hay ký tự (không phải số) các thông tin Height và Weight 
+        public override void InputHeight()
+        {
+            while (true)
+            {
+                try
+                {
+                    Console.Write("Input Dog height (cm): ");
+                    height = int.Parse(Console.ReadLine());
+                    break;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Not input a Number. Please reinput a number!");
+                }
+            }
+        }
+        public override void InputWeight()
+        {
+            while (true)
+            {
+                try
+                {
+                    Console.Write("Input Dog weight (kg): ");
+                    weight = int.Parse(Console.ReadLine());
+                    break;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Not input a Number. Please reinput a number!");
+                }
+            }
+        }
+        public override void DisplayInfo()
+        {
+            Console.WriteLine("Dog Name = {0}, Age = {1}, " +
+                "Height = {2} (cm), Weight = {3} (kg)", name, age, height, weight);
+        }
+        public override string ToString()
+        {
+            return "Dog " + base.ToString();
+        }
+    }
+
+    public class Cat : Animal
+    {
+        public Cat() : base() { }
+        public Cat(string name, int age, int height, double weight) : base(name, age, height, weight) { }
+        public override void InputInfo()
+        {
+            Console.Write("Input Cat name: ");
+            this.name = Console.ReadLine();
+            InputAge();
+            InputHeight();
+            InputWeight();
+        }
+        public override void InputAge()
+        {
+            while (true)
+            {
+                try
+                {
+                    Console.Write("Input Cat Age: ");
+                    age = int.Parse(Console.ReadLine());
+                    if (age <= 0 || age > 20) throw new InvalidNumException("Age must be between 1 and 20.");
+                    break;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Not input a Number. Please reinput a number!");
+                }
+                catch (InvalidNumException e)
+                {
+                    Console.WriteLine("Invalid Number: {0}", e.Message);
+                }
+            }
+        }
+        //d. Bắt lỗi khi người dùng nhập chuỗi hay ký tự (không phải số) các thông tin Height và Weight 
+        public override void InputHeight()
+        {
+            while (true)
+            {
+                try
+                {
+                    Console.Write("Input Cat height (cm): ");
+                    height = int.Parse(Console.ReadLine());
+                    break;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Not input a Number. Please reinput a number!");
+                }
+            }
+        }
+        public override void InputWeight()
+        {
+            while (true)
+            {
+                try
+                {
+                    Console.Write("Input Cat weight (kg): ");
+                    weight = int.Parse(Console.ReadLine());
+                    break;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Not input a Number. Please reinput a number!");
+                }
+            }
+        }
+        public override void DisplayInfo()
+        {
+            Console.WriteLine("Cat Name = {0}, Age = {1}, " +
+                "Height = {2} (cm), Weight = {3} (kg)", name, age, height, weight);
+        }
+        public override string ToString()
+        {
+            return "Cat " + base.ToString();
+        }
+    }
+    
+}
---
+++ Lab_01/SportManagement/SportManagement/Program.cs
@@ -0,0 +1,188 @@
+﻿using MainData;
+using System.Runtime.CompilerServices;
+
+namespace SportManagement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Nhập 3 môn thể thao
+            Sport football = new Football();
+            football.InputInfo();
+            Sport tennis = new Tennis();
+            tennis.InputInfo(); 
+            Sport volleyball = new Volleyball();
+            volleyball.InputInfo();
+
+            //xuất 3 môn thể thao
+            football.DisplayInfo();
+            tennis.DisplayInfo();   
+            volleyball.DisplayInfo();
+        }
+    }
+}
+
+namespace MainData
+{
+    //dùng để bắt lỗi khi nhập quá số người chơi cho phép của môn thể thao
+    public class InvalidNumException : Exception
+    {
+        public InvalidNumException(string message) : base(message) { }
+    }
+    public abstract class Sport
+    {
+        protected string name;
+        protected int playerQuantity; //số lượng người chơi
+        protected string timePlay; //thời gian chơi, để string do có thể tính set hoặc phút 
+        protected string ballType; //loại bóng
+        public Sport() { }
+        public Sport(int playerQuantity, string timePlay, string ballType)
+        {
+            this.playerQuantity = playerQuantity;
+            this.timePlay = timePlay;
+            this.ballType = ballType;
+        }
+        public virtual void InputInfo()
+        {
+            Console.WriteLine("Enter information of {0}", name);
+            InputPlayerQuantity();
+            Console.Write("Input time play: ");
+            this.timePlay = Console.ReadLine();
+            Console.Write("Input ball type: ");
+            this.ballType = Console.ReadLine();
+        }
+        public abstract void InputPlayerQuantity();
+        public virtual void DisplayInfo()
+        {
+            Console.WriteLine("Name = {0}, Player Quantity = {1}, Time = {2}, Ball Type = {3}", name, playerQuantity, timePlay, ballType);
+        }
+
+        public override string ToString()
+        {
+            return "Name = " + name + ", Player Quantity = " + playerQuantity + ", Time = " + timePlay
+                    + ", Ball Type = " + ballType + "\n";
+        }
+    }
+    public class Football : Sport
+    {
+
+        public Football() : base()
+        {
+            this.name = "Football";
+        }
+        public Football(int playerQuantity, string timePlay, string ballType) : base(playerQuantity, timePlay, ballType)
+        {
+            this.name = "Football";
+        }
+        public override void InputPlayerQuantity()
+        {
+            while (true)
+            {
+                try
+                {
+                    Console.Write("Input player quantity: ");
+                    string str = Console.ReadLine();
+                    playerQuantity = int.Parse(str);
+                    if (playerQuantity <= 0 || playerQuantity > 22) //vì mỗi đội bóng đá có tối đa 11 người
+                    {
+                        throw new InvalidNumException("The number of players must be between 1 and 22");
+                    }
+                    break;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Input not a number. Please reinput a number!!!!");
+                }
+                catch (InvalidNumException e)
+                {
+                    Console.WriteLine("Invalid number: {0}", e.Message);
+                }
+            }
+        }
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+    public class Tennis : Sport
+    {
+        public Tennis() : base()
+        {
+            this.name = "Tennis";
+        }
+        public Tennis(int playerQuantity, string timePlay, string ballType) : base(playerQuantity, timePlay, ballType)
+        {
+            this.name = "Tennis";
+        }
+        public override void InputPlayerQuantity()
+        {
+            while (true)
+            {
+                try
+                {
+                    Console.Write("Input player quantity: ");
+                    string str = Console.ReadLine();
+                    playerQuantity = int.Parse(str);
+                    if (playerQuantity <= 0 || playerQuantity > 4) //vì mỗi đội tennish có tối đa 2 người
+                    {
+                        throw new InvalidNumException("The number of players must be between 1 and 4");
+                    }
+                    break;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Input not a number. Please reinput a number!!!!");
+                }
+                catch (InvalidNumException e)
+                {
+                    Console.WriteLine("Invalid number: {0}", e.Message);
+                }
+            }
+        }
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+    public class Volleyball : Sport
+    {
+        public Volleyball() : base()
+        {
+            this.name = "Volleyball";
+        }
+        public Volleyball(int playerQuantity, string timePlay, string ballType) : base(playerQuantity, timePlay, ballType)
+        {
+            this.name = "Volleyball";
+        }
+        public override void InputPlayerQuantity()
+        {
+            while (true)
+            {
+                try
+                {
+                    Console.Write("Input player quantity: ");
+                    string str = Console.ReadLine();
+                    playerQuantity = int.Parse(str);
+                    if (playerQuantity <= 0 || playerQuantity > 12) //vì mỗi đội bóng chuyền có tối đa 6 người
+                    {
+                        throw new InvalidNumException("The number of players must be between 1 and 12");
+                    }
+                    break;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Input not a number. Please reinput a number!!!!");
+                }
+                catch (InvalidNumException e)
+                {
+                    Console.WriteLine("Invalid number: {0}", e.Message);
+                }
+            }
+        }
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
